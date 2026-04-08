92db26d248dd9700a565c4fa326fb009d9134979
Prem Mehta <premarmehta@gmail.com>
2026-04-08 12:37:13 +0530

    exp-9_dunn_successfully!

---
+++ Exp_9/counter-app/src/counter.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
+
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </div>
+  );
+}
+
+export default Counter;
